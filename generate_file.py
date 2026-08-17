6168085d970db1fd1c8cefef5fced1324ca3fa91
bestlabz <bestlabzglobal@gmail.com>
2026-08-17 18:19:12 +0530

    refactor: remove unnecessary whitespace in generate_file.py

--- generate_file.py
+++ generate_file.py
@@ -2,7 +2,6 @@ from magic_write import MagicWriteModel
 import json
 
 model = MagicWriteModel()
-
 result = model.generate(
     "Sparkle",
     count=12,
@@ -10,5 +9,4 @@ result = model.generate(
     output_type="fabric",
     generation_mode="modern_composition",
 )
-
 print(json.dumps(result, indent=2))
